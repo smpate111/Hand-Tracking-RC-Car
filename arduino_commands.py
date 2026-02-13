0b4b6cad86038dd01e275e9dac12f15c4240fda3
Sahil Patel <sahilp1424@gmail.com>
2026-02-12 19:30:10 -0700

    testing again

--- arduino_commands.py
+++ arduino_commands.py
@@ -26,9 +26,6 @@ class hand_to_command:
     
     # dummy function
     def _testing_(self):
-        print("qwe")
-        print("123")
-        print("321")
         return
     
     # determine the command based on which hand is active inside the hand box
