98d03bc678a6072784dcce4ef7aaf2fc102d3ec5
Sahil Patel <sahilp1424@gmail.com>
2026-02-14 10:26:42 -0700

    updated hand_configurations.py

--- hand_configurations.py
+++ hand_configurations.py
@@ -41,12 +41,14 @@ f_height = 480
 # change these to whatever values you want (these are set to mirror the left box)
 new_x1 = f_width - x2
 new_x2 = f_width - x1
+new_y1 = y1
+new_y2 = y2
 
 # define it here so that main.py and hand_visualizer.py can see them
 x1 = new_x1
-y1 = y1
+y1 = new_y1
 x2 = new_x2
-y2 = y2
+y2 = new_y2
 RIGHT_BOX = {"x1": x1, "y1": y1, "x2": x2, "y2": y2}
 # the right box is calculated dynamically in main.py/hand_visualizer.py based on frame width
 # can hardcode the right box here if the frame size is constant
